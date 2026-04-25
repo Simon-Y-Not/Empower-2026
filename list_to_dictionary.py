c8a673437053938819e3b290aeb39f472d312073
Simon Sebastian <simon@y-not.com>
2026-04-25 12:37:32 +0530

    Added list to dictionary Python script

---
+++ list_to_dictionary.py
@@ -0,0 +1,10 @@
+colour_list = ["Yellow", "Blue", "Red", "Black", "Orange", "White"]
+
+colour_dictionary = dict()
+position = 1
+
+for item in colour_list:
+    colour_dictionary[position] = item
+    position += 1
+
+print(colour_dictionary)
